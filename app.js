2f9808a31af2276f573506d763e4c51a1c0ed528
Keovkevin <shubhamshekhar089@gmail.com>
2017-09-18 23:34:29 +0530

    half way done with this feature.Tested so far,so good.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+function newFeature(){
+	console.log("Writing new features");
+	console.log("Not done with this feature");
+};
+
 function something() {
 	console.log("I\'m something");
 }
 console.log("Hello World!");
 something();
+newFeature();
 
