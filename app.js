20c0180873eb4905c0c7f602c004dfe13c019e9f
Keovkevin <shubhamshekhar089@gmail.com>
2017-09-18 23:51:25 +0530

    finally added a new-feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function newFeature(){
 	console.log("Writing new features");
 	console.log("Not done with this feature");
+	console.log("Finally Completely done with this new feature");
 };
 
 function something() {
