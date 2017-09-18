c17dd0c345725df3c8f380ca5d67aa3b5a8556de
Keovkevin <shubhamshekhar089@gmail.com>
2017-09-19 00:12:33 +0530

    final

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function newFeature(){
 
 function something() {
   	console.log("I\'m something");
-	console("Bug fixed!");
+	console.log("Bug fixed!");
 }
 console.log("Hello World!");
 something();
